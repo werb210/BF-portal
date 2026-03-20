71e18acf93e63d5dfeb359e9eb10b4441b6e9676
werb210 <todd.w@boreal.financial>
2026-03-20 12:22:02 -0600

    harden vitest setup with msw server hooks

---
+++ src/test/server.ts
@@ -0,0 +1,7 @@
+import { http, HttpResponse } from 'msw'
+import { setupServer } from 'msw/node'
+
+export const server = setupServer(
+  http.get('/api/{*path}', () => HttpResponse.json({ data: [] }, { status: 200 })),
+  http.post('/api/{*path}', () => HttpResponse.json({ success: true }, { status: 200 }))
+)
