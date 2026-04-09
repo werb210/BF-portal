020a00238c3f1ddbd71bdca6ed753e2d78cfc92b
werb210 <todd.w@boreal.financial>
2026-04-09 15:50:08 -0600

    test: wire msw server into setup and mock auth me

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom";
+import { server } from "./msw/server";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
